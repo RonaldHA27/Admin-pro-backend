4e1417f20a328e5e9b567e24e8a1646215ecacfb
Ronald HA <ronaldhuamanatalaya@gmail.com>
2022-06-20 15:35:39 -0500

    Fin de Sección 10- Auth

--- index.js
+++ index.js
@@ -8,25 +8,21 @@ const {dbConnection} = require('./database/config');
 //Crear el servidor express
 const app = express(); 
 
-//Base de datos
-dbConnection();
-
 //Configurar Cors
 app.use(cors());
 
-//zW8pZveT3hVC8r5V
+//Lectura y parseo del body
+app.use(express.json() );
 
-
+//Base de datos
+dbConnection();
 //Rutas 
 
-app.get( '/',(req, res) =>{
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/login', require('./routes/auth'));
 
-    res.json({
-        ok:true,
-        msg:"Hola Mundo"
-    })
 
-})
+
 
 app.listen(process.env.PORT, ()=> {
     console.log('servidor corriendo en puerto ' + 3000);
